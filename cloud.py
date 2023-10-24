4e74007ad9bdc40507da3a94f3d391a6c8e9840e
Jon Mentzell <jmentzell@redhat.com>
2023-10-24 16:24:48 -0400

    removed some ambiguity re: file

--- cloud.py
+++ cloud.py
@@ -3,10 +3,10 @@ import os
 from os import path
 from wordcloud import WordCloud
 
-file = input("What file of text do you want to make a wordcloud?\n")
-outfile = file.replace(".txt", ".png")
+filename = input("What file of text do you want to make a wordcloud?\n")
+outfile = filename.replace(".txt", ".png")
 
-with open(file) as this_file:
+with open(filename) as this_file:
     text = this_file.read()
 
 wordcloud = WordCloud(width = 1600, height = 1200,
