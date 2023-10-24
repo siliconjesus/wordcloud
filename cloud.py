6ee0ec0aef97db3f62ac135efb7f95d0cdd00983
Jon Mentzell <jmentzell@redhat.com>
2023-10-24 14:25:35 -0400

    remove try - moved var out of scope.

--- cloud.py
+++ cloud.py
@@ -7,10 +7,7 @@ d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 file = input("What file of text do you want to make a wordcloud? ")
 outfile = file.replace(".txt", ".png")
 
-try:
-    text = open(path.join(d, file)).read()
-except:
-    print("error opening file")
+text = open(path.join(d, file)).read()
 
 wordcloud = WordCloud(random_state=1, regexp=r'[a-zA-Z\/]').generate(text)
 wordcloud.to_file(outfile)
